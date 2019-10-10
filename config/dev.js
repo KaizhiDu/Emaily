529ab64f926d77f3c3ff4d7a84822dc0c046ed0d
KAIZHIDU <529443744@qq.com>
2019-10-09 17:49:02 -0700

    commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js do not commit it
+module.exports = {
+    googleClientID: '1063834274117-hb6ned03n0lq726jhl1afpe6bjprgin3.apps.googleusercontent.com',
+    googleClientSecret: 'DSInCeHxEfsZk6b32hWBONLF',
+    mongoURI: 'mongodb+srv://admin-kevin:2wnKt6CRkwyMbngV@emily-o9czr.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'asdfaelqrksaghlaskgjlaskdgjljkjasdfasdf'
+}
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-// call express from libraries
 const express = require('express');
+const mongoose = require('mongoose');
+const cookiesSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 const app = express();
 
-// this is a route
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+app.use(
+    cookiesSession({
+        maxAge: 24 * 60 *60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./models/User');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+authRoutes(app);
+
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;
 
